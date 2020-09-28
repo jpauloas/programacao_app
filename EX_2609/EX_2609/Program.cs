9e601bdfefcd3a880871983459ecc93cfea057aa
jpauloas <joaopaulosouto@outlook.com>
2020-09-27 23:16:29 -0300

    Atv 26/09

---
+++ EX_2609/EX_2609/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace EX_2609
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  1 )  Crie uma variável e imprima na tela cada um dos tipos de dados da tabela  */
+            string ex01 = "bool" +
+                "   byte " +
+                "   sbyte" +
+                "   char  " +
+                "   decimal " +
+                "   double" +
+                "   float" +
+                "   int" +
+                "   uint" +
+                "   long" +
+                "   ulong" +
+                "   short" +
+                "   ushort";
+            Console.WriteLine(ex01);
+
+            /*  2 ) Exiba na tela o número da sua casa  */
+            string ex02 = "Numero:  310";
+            Console.WriteLine(ex02);
+
+            /*  3 ) Exiba na tela o número de habitantes da terra.   */
+            string ex03 = "Numero de habitantes:  7.735.084.676";
+            Console.WriteLine(ex03);
+
+            /*  4 ) Imprima na tela o valor do salário que você gostaria de receber. */
+            string ex04 = "Salário:   3500";
+            Console.WriteLine(ex04);
+
+            /*  5 ) Exiba na tela o número do seu telefone celular*/
+            string ex05 = "Celular:   31996073528";
+            Console.WriteLine(ex05);
+
+            /*  6 ) Imprima na tela o seu nome completo */
+            string ex06 = "Nome:  João Paulo Araujo Souto";
+            Console.WriteLine(ex06);
+
+            /*  7 ) Faça um comentário qualquer no seu código. */
+            /* Questao 03: comentário */
+
+            /*  8 ) Utilizando Unicode e o tipo de dados char. Imprima na tela a letra R.*/
+            char ex08 = U + 0052;
+            Console.WriteLine(ex08);
+
+            /*  9 ) Exiba na tela o valor máximo do data type ulong */
+            string ex09 = "O valor máximo para tipo Ulong é de: 18,446,744,073,709,551,615";
+            Console.WriteLine(ex09);
+
+            /* 10 ) Exiba na tela o valor mínimo do data type long */
+            string ex10 = "O valor minimo para tipo Long é de: -9,223,372,036,854,775,808";
+            Console.WriteLine(ex10);
+        }
+    }
+}
---
+++ aula0925/aula0925/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace aula0925
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*TipodeDado nomeDaVariavel = valor; */
+            sbyte x = 1;
+            
+            Console.WriteLine("Hello World!"); /*escreve*/
+            Console.WriteLine(x); /*exibe o valor de x*/
+
+            byte y = 255;
+            y++; /*adiciona mais um a variavel*/
+            Console.WriteLine(y); /*vai imprimir 0, pois y so suporta ate 255 como byte*/
+
+            sbyte z = 127;
+            z++;
+            Console.WriteLine(z)
+
+            long n3 = 2147483648L;
+            Console.WriteLine(n2);
+
+            bool completo = false;
+            Console.WriteLine(completo);
+
+            bool incompleto = true;
+            Console.WriteLine(incompleto);
+
+            char genero = 'M';
+            Console.WriteLine(incompleto);
+
+            char letra = '\u0057';
+            Console.WriteLine(letra);
+
+            float n5 = 4.2f; /*precisa colocar f dps pois indica floatinh*/
+
+            double n4 = 5.9;
+            Console.WriteLine (n4)
+
+            string nome = "João Paulo"; /*qnd um caracter aspa simples, mais de um aspas duplas*/
+            Console.WriteLine(nome)
+
+            nome = "Paulo"; /*nao declara dnv, apenas usa*/
+
+            object obj1 = "nome";
+
+            object obj2 = 5.7; /*recebe sem precisar declarar
+
+
+
+
+
+
+
+        }
+    }
+}
