86e1661f55af800eff9458c56bfc0196cf308f55
jpauloas <joaopaulosouto@outlook.com>
2020-11-13 13:31:31 -0300

    Atv015

---
+++ ATV015/ATV015/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATV015
+{
+    class Retangulo
+    {
+        private double _largura;
+        private double _altura;
+
+
+        public Retangulo(double largura, double altura)
+        {
+            _largura = largura;
+            _altura = altura;
+        }
+        public double GetLargura()
+        {
+            return _largura;
+        }
+        public void SetLargura(double largura)
+        {
+            _largura = largura;
+        }
+        public double GetAltura()
+        {
+            return _altura;
+        }
+        public void SetAltura(double altura)
+        {
+            _altura = altura;
+        }
+        public double Area()
+        {
+            return _largura * _altura;
+        }
+        public double Perimetro()
+        {
+            return 2 * (_largura + _altura);
+        }
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(_largura, 2.0) + Math.Pow(_altura, 2.0));
+        }
+        public override string ToString()
+        {
+            return "Valor da area: " + Area().ToString("F2", CultureInfo.InvariantCulture) +
+                "Valor do Perímetro " + Perimetro().ToString("F2", CultureInfo.InvariantCulture) +
+                "Valor da Diagonal" + Diagonal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
---
+++ ATV015/ATV015/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ATV015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercicio A //
+            Console.Write("Progerama que calcula a area, periometro e diagonal de um retangulo");
+            Console.WriteLine();
+            Retangulo r = new Retangulo(3.0, 5.0);
+            r.SetLargura(2.0);
+            r.SetAltura(5.0);
+            Console.WriteLine(r.GetLargura());
+            Console.WriteLine(r.GetAltura());
+
+            // Exercicio B //
+            Console.WriteLine("Programa de controladoria de estoque");
+            Console.WriteLine();
+            Produto p = new Produto("TV", 500.00, 10);
+            Console.WriteLine(p.GetNome());
+            Console.WriteLine(p.GetPreco());
+            Console.WriteLine(p.GetQuantidade());
+
+            // Exercicio C //
+            Professor p = new Professor("Aguinaldo", "Souza", 35334444, true);
+            p.SetOculos(false);
+            Console.WriteLine(p.GetNome());
+            Console.WriteLine(p.GetSobrenome());
+            Console.WriteLine(p.GetTelefone());
+            Console.WriteLine(p.GetOculos());
+
+            // Exercicio D //
+            Aluno a = new Aluno("Carolina", "carolina@gmail.com", "Belo Horizonte", true);
+            a.SetNome("Bruna");
+            a.SetEmail("bruna@gmail.com");
+            a.SetCidade("Nova Lima");
+            a.SetStatus(false);
+            Console.WriteLine(a.GetNome());
+            Console.WriteLine(a.GetEmail());
+            Console.WriteLine(a.GetCidade());
+            Console.WriteLine(a.GetStatus());
+
+        }
+    }
+}
